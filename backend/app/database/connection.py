4e32c511efb60e9025e30afe29e343ab23f0daba
NguyenAnhTruong2K5 <nguyenanhtruong2k5@gmail.com>
2025-10-11 18:34:28 +0700

    Update connection.py

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -3,9 +3,9 @@ import mysql.connector
 def get_connection():
     connection = mysql.connector.connect(
         host="localhost",       # your MySQL host (local machine)
-        port=3306,              # default MySQL port
-        user="root",            # your MySQL username
-        password="yourpassword",# your MySQL password
-        database="my_database"  # your database name
+        port=3306,                
+        user="root",           
+        password="yourpassword",# Điền mật khẩu vào đây
+        database="QuanLyBaiDoXe" 
     )
     return connection
