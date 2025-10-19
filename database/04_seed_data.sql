f922888a675a02a95411cf2c04a17ab1f9f4ac32
QuangMinh2005 <minhdragonmcgamer@gmail.com>
2025-10-19 14:43:36 +0700

    Update 04_seed_data.sql

--- database/04_seed_data.sql
+++ database/04_seed_data.sql
@@ -5,10 +5,10 @@
 -- Bảng Pricing: 4 loại giá
 INSERT INTO Pricing (pricing_id, vehicle_type, term, rate)
 VALUES
-(1, 'motorbike', 'hourly', 3000),   -- 3k/h
-(2, 'car', 'hourly', 10000),        -- 10k/h
-(3, 'motorbike', 'monthly', 300000),-- 300k/tháng
-(4, 'car', 'monthly', 1000000);     -- 1000k/tháng
+('1', 'motorbike', 'hourly', 3000),   -- 3k/h
+('2', 'car', 'hourly', 10000),        -- 10k/h
+('3', 'motorbike', 'monthly', 300000),-- 300k/tháng
+('4', 'car', 'monthly', 1000000);     -- 1000k/tháng
 GO
 
 -- Bảng ParkingSlots: 3 bãi A, B, C mỗi bãi 100 chỗ
