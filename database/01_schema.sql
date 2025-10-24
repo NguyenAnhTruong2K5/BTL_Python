036a53480d88651889a4d1a491503a00a9351771
QuangMinh2005 <minhdragonmcgamer@gmail.com>
2025-10-24 15:05:03 +0700

    Refactor database schema to use phone numbers as keys
    Chỉnh sửa khóa chính của Customer và Vehicle thành phone number và plate number

--- database/01_schema.sql
+++ database/01_schema.sql
@@ -1,6 +1,3 @@
--- ========================================
--- Database
--- ========================================
 CREATE DATABASE ParkingManagement;
 GO
 
@@ -10,10 +7,7 @@ GO
 -- ========================================
 -- SEQUENCES
 -- ========================================
-CREATE SEQUENCE seq_customer START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE seq_vehicle START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE seq_slot START WITH 1 INCREMENT BY 1;
-CREATE SEQUENCE seq_contract START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE seq_card START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE seq_record START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE seq_invoice START WITH 1 INCREMENT BY 1;
@@ -23,24 +17,20 @@ GO
 -- Customers
 -- ========================================
 CREATE TABLE Customer (
-    customer_id VARCHAR(20) PRIMARY KEY 
-        DEFAULT ('CUST' + RIGHT('000000' + CAST(NEXT VALUE FOR seq_customer AS VARCHAR(6)), 6)),
-    name NVARCHAR(100),
-    phone_number NVARCHAR(20),
+    phone_number NVARCHAR(20) PRIMARY KEY,
+    name NVARCHAR(100) NOT NULL,
     email NVARCHAR(100)
 );
 GO
 
 -- ========================================
--- Vehicle
+-- Vehicles
 -- ========================================
 CREATE TABLE Vehicle (
-    vehicle_id VARCHAR(20) PRIMARY KEY 
-        DEFAULT ('VEHI' + RIGHT('000000' + CAST(NEXT VALUE FOR seq_vehicle AS VARCHAR(6)), 6)),
-    customer_id VARCHAR(20) NOT NULL,
+    plate_number NVARCHAR(20) PRIMARY KEY,
+    phone_number NVARCHAR(20) NOT NULL,
     vehicle_type NVARCHAR(20) NOT NULL CHECK(vehicle_type IN ('motorbike','car')),
-    plate_number NVARCHAR(20) UNIQUE,
-    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+    FOREIGN KEY (phone_number) REFERENCES Customer(phone_number)
 );
 GO
 
@@ -62,7 +52,7 @@ GO
 CREATE TABLE Pricing (
     pricing_id VARCHAR(20) PRIMARY KEY,
     vehicle_type NVARCHAR(20) NOT NULL CHECK(vehicle_type IN ('motorbike','car')),
-    term NVARCHAR(10) NOT NULL CHECK(term IN ('hourly','monthly', 'yearly')),
+    term NVARCHAR(10) NOT NULL CHECK(term IN ('hourly','monthly','yearly')),
     rate DECIMAL(18,2) NOT NULL
 );
 GO
@@ -71,16 +61,16 @@ GO
 -- Contracts
 -- ========================================
 CREATE TABLE Contract (
-    vehicle_id VARCHAR(20) PRIMARY KEY,      -- Vehicle là khóa chính (1 xe 1 hợp đồng)
+    plate_number NVARCHAR(20) PRIMARY KEY,       -- mỗi xe chỉ có 1 hợp đồng tại 1 thời điểm
+    phone_number NVARCHAR(20) NOT NULL,
     pricing_id VARCHAR(20) NOT NULL,
-    customer_id VARCHAR(20) NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NULL,
-    term NVARCHAR(10) CHECK (term IN ('monthly','yearly')) NOT NULL,    -- thời hạn
-    duration INT NOT NULL,   -- số tháng/năm
+    term NVARCHAR(10) CHECK (term IN ('monthly','yearly')) NOT NULL,
+    duration INT NOT NULL,
     FOREIGN KEY (pricing_id) REFERENCES Pricing(pricing_id),
-    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
-    FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id)
+    FOREIGN KEY (plate_number) REFERENCES Vehicle(plate_number),
+    FOREIGN KEY (phone_number) REFERENCES Customer(phone_number)
 );
 GO
 
@@ -91,9 +81,9 @@ CREATE TABLE Card (
     card_id VARCHAR(20) PRIMARY KEY 
         DEFAULT ('CARD' + RIGHT('000000' + CAST(NEXT VALUE FOR seq_card AS VARCHAR(6)), 6)),  
     card_qr NVARCHAR(255) NOT NULL UNIQUE,
-    vehicle_id VARCHAR(20) NULL,
+    plate_number NVARCHAR(20) NULL,
     status NVARCHAR(20) CHECK (status IN ('active','inactive','lost')) DEFAULT 'inactive',
-    FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id)
+    FOREIGN KEY (plate_number) REFERENCES Vehicle(plate_number)
 );
 GO
 
@@ -105,13 +95,13 @@ CREATE TABLE ParkingRecord (
         DEFAULT ('RECO' + RIGHT('000000' + CAST(NEXT VALUE FOR seq_record AS VARCHAR(6)), 6)),
     card_id VARCHAR(20) NOT NULL,
     slot_id VARCHAR(20) NOT NULL,
-    vehicle_id VARCHAR(20) NOT NULL,
+    plate_number NVARCHAR(20) NOT NULL,
     check_in_time DATETIME NOT NULL,
     check_out_time DATETIME NULL,
     hours INT NULL,
     fee DECIMAL(18,2) NULL,
     FOREIGN KEY (slot_id) REFERENCES ParkingSlot(slot_id),
-    FOREIGN KEY (vehicle_id) REFERENCES Vehicle(vehicle_id),
+    FOREIGN KEY (plate_number) REFERENCES Vehicle(plate_number),
     FOREIGN KEY (card_id) REFERENCES Card(card_id)
 );
 GO
