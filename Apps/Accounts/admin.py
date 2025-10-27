356142ad1528b4075f7276a1af51d29049131890
NguyenAnhTruong2K5 <nguyenanhtruong2k5@gmail.com>
2025-10-27 14:03:24 +0700

    Update admin.py

--- Apps/Accounts/admin.py
+++ Apps/Accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Customer, Contract
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('cccd', 'name', 'email', 'phone_number')
+    search_fields = ('cccd', 'name', 'email', 'phone_number')
+    ordering = ('-name', )
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ('plate_number', "vehicle_type", 'term', 'duration', 'start_date', 'end_date', )
+    search_fields = ('start_date', 'end_date', )
+    list_filter = ('term', 'duration', )
+    ordering = ('-start_date', )
+    readonly_fields = ('start_date', 'end_date')
+
+    # This organizes your add/edit page
+    fieldsets = (
+        (None, {
+            'fields': ('plate_number', 'cccd', 'term', 'duration', 'vehicle_type')
+        }),
+        ('Calculated Dates (Read-Only)', {
+            'fields': ('start_date', 'end_date')
+        }),
+    )
