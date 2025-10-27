1853930c8f3f5f2d4ff122d5d09831c7b82909d0
NguyenAnhTruong2K5 <nguyenanhtruong2k5@gmail.com>
2025-10-27 14:05:06 +0700

    Update models.py

--- Apps/Accounts/models.py
+++ Apps/Accounts/models.py
@@ -1,22 +1,27 @@
 from django.db import models
-
+from dateutil.relativedelta import relativedelta
+from django.utils import timezone
 class Customer(models.Model):
-    customer_id = models.AutoField(primary_key=True)
-    full_name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=15, unique=True)
-    email = models.EmailField(unique=True, null=True, blank=True)
-    address = models.CharField(max_length=255, null=True, blank=True)
-
-    def __str__(self):
-        return self.full_name
+    cccd = models.CharField(max_length=20, primary_key= True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length= 100, null= True,)
+    phone_number = models.CharField(max_length=20, unique=True, null= False)
 
 
 class Contract(models.Model):
-    contract_id = models.AutoField(primary_key=True)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="contracts")
-    start_date = models.DateField()
-    end_date = models.DateField(null=True, blank=True)
-    status = models.CharField(max_length=20, default="active")  # active, expired, cancelled
+    plate_number = models.CharField(max_length=20, primary_key= True)
+    vehicle_type = models.CharField(max_length=100, null= False, blank= False)
+    cccd = models.OneToOneField('Customer', on_delete=models.CASCADE, to_field= 'cccd', db_column= 'cccd')
+    term = models.CharField(max_length=10, choices= [('monthly', 'Monthly'), ('yearly', 'Yearly')])
+    duration = models.IntegerField()
+    start_date = models.DateField(default= timezone.now)
+    end_date = models.DateField(blank= True, editable= False, null= True)
 
-    def __str__(self):
-        return f"Contract {self.contract_id} - {self.customer.full_name}"
+    def save(self, *args, **kwargs):
+        print("Calculating end_date based on term and duration...")
+        if self.term == 'monthly':
+            self.end_date = self.start_date + relativedelta(months= self.duration)
+        elif self.term == 'yearly':
+            self.end_date = self.start_date + relativedelta(years= self.duration)
+        super().save(*args, **kwargs)
+
