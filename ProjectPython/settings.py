fe5a6066230ae4fc180d9e14bc92da60a508b660
NguyenAnhTruong2K5 <nguyenanhtruong2k5@gmail.com>
2025-10-27 13:55:09 +0700

    Update settings.py

--- ProjectPython/settings.py
+++ ProjectPython/settings.py
@@ -117,6 +117,6 @@ STATICFILES_DIRS = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "database" / "db.sqlite3",
     }
 }
