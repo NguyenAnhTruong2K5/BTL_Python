da62bf037b3b62d1709825be5fe5d235fd656beb
NguyenAnhTruong2K5 <nguyenanhtruong2k5@gmail.com>
2025-10-29 09:22:14 +0700

    Update serializers.py

--- Apps/Accounts/serializers.py
+++ Apps/Accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Customer, Contract
-
+from django.utils import timezone
 #Serializers for Customer:
 class ListCreateRetrieveDestroyCustomerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,18 +13,26 @@ class UpdateCustomerSerializer(serializers.ModelSerializer):
         model = Customer
         fields = ["phone_number", "email"]
 
-
 #Serializers for Contract:
 class CreateContractSerializer(serializers.ModelSerializer):
+    cccd = serializers.SlugRelatedField(
+        slug_field= 'cccd',
+        read_only= True
+    )
+
     class Meta:
         model = Contract
-        fields = ['plate_number', 'vehicle_type', 'term', 'duration']
+        fields = ['cccd', 'plate_number', 'vehicle_type', 'term', 'duration']
 
 
 class ListContractSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contract
         exclude = ["cccd"]
+        def get_status(self, obj):
+            if obj.end_date < timezone.now():
+                return 'expired'
+            return obj.status
 
 
 class UpdateContractSerializer(serializers.ModelSerializer):
