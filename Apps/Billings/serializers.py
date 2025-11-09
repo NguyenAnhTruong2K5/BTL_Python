90e7a76163bd0150fce9e6f253e6f162ebf82c2f
DucHieu1512 <hieuzeus101120@gmail.com>
2025-11-09 21:16:54 +0700

    Billingzzz

--- Apps/Billings/serializers.py
+++ Apps/Billings/serializers.py
@@ -1,36 +1,68 @@
 from rest_framework import serializers
-from .models import ContractInvoice, ParkingInvoice, Contract, ParkingRecord, Pricing
+from django.utils import timezone
+from .models import Contract_Invoice, Parking_Invoice, Pricing
+from Apps.Parkings.models import ParkingRecord
 
 # Contract Invoice
+
 class ContractInvoiceSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ContractInvoice
+        model = Contract_Invoice
         fields = '__all__'
 
 
 class CreateContractInvoiceSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ContractInvoice
-        fields = ['pricing', 'amount']
+        model = Contract_Invoice
+        fields = ['pricing']
 
     def create(self, validated_data):
-        invoice = ContractInvoice.objects.create(**validated_data)
+        pricing = validated_data['pricing']
+
+        if pricing.term == 'monthly':
+            amount = pricing.rate
+        elif pricing.term == 'yearly':
+            amount = pricing.rate * 12
+        else:
+            amount = pricing.rate
+
+        invoice = Contract_Invoice.objects.create(
+            pricing=pricing,
+            amount=amount,
+            payment_date=timezone.now()
+        )
         return invoice
+
 
 
 # Parking Invoice
 
 class ParkingInvoiceSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ParkingInvoice
+        model = Parking_Invoice
         fields = '__all__'
 
 
 class CreateParkingInvoiceSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ParkingInvoice
-        fields = ['record', 'pricing', 'amount']
+        model = Parking_Invoice
+        fields = ['record', 'pricing']
 
     def create(self, validated_data):
-        invoice = ParkingInvoice.objects.create(**validated_data)
+        record = validated_data['record']
+        pricing = validated_data['pricing']
+
+        if record.exit_time and record.entry_time:
+            duration = (record.exit_time - record.entry_time).total_seconds() / 3600
+        else:
+            duration = 0
+
+        amount = round(duration * float(pricing.rate), 2)
+
+        invoice = Parking_Invoice.objects.create(
+            record=record,
+            pricing=pricing,
+            amount=amount,
+            payment_date=timezone.now()
+        )
         return invoice
--- Apps/Billings/views.py
+++ Apps/Billings/views.py
@@ -1,71 +1,86 @@
-﻿from rest_framework import generics
+﻿from rest_framework import generics, status
 from rest_framework.response import Response
-from .models import ContractInvoice, ParkingInvoice
-from .serializers import (ContractInvoiceSerializer,CreateContractInvoiceSerializer,ParkingInvoiceSerializer,CreateParkingInvoiceSerializer,)
+from .models import Contract_Invoice, Parking_Invoice
+from .serializers import (
+    ContractInvoiceSerializer,
+    CreateContractInvoiceSerializer,
+    ParkingInvoiceSerializer,
+    CreateParkingInvoiceSerializer,
+)
 
-# ContractInvoice Views
+
+
+# Contract invoice
 
 class ContractInvoiceListCreateView(generics.ListCreateAPIView):
-    queryset = ContractInvoice.objects.all()
+    queryset = Contract_Invoice.objects.all()
     serializer_class = ContractInvoiceSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = CreateContractInvoiceSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         invoice = serializer.save()
-        return Response(ContractInvoiceSerializer(invoice).data)
+        return Response(
+            {
+                "invoice": ContractInvoiceSerializer(invoice).data
+            },
+            status=status.HTTP_201_CREATED
+        )
 
 
 class ContractInvoiceRetrieveUpdateView(generics.RetrieveUpdateAPIView):
-    queryset = ContractInvoice.objects.all()
+    queryset = Contract_Invoice.objects.all()
     serializer_class = ContractInvoiceSerializer
     lookup_field = 'invoice_id'
 
 
 class ContractInvoiceDestroyView(generics.DestroyAPIView):
-    queryset = ContractInvoice.objects.all()
+    queryset = Contract_Invoice.objects.all()
     serializer_class = ContractInvoiceSerializer
     lookup_field = 'invoice_id'
 
 
 class SearchContractInvoiceView(generics.ListAPIView):
-    queryset = ContractInvoice.objects.all()
     serializer_class = ContractInvoiceSerializer
 
     def get_queryset(self):
         search_term = self.request.GET.get('search', '')
-        return ContractInvoice.objects.filter(pricing__pricing_id__icontains=search_term)
+        return Contract_Invoice.objects.filter(pricing__pricing_id__icontains=search_term)
 
 
-# ParkingInvoice Views
+# Parking Invoice
 
 class ParkingInvoiceListCreateView(generics.ListCreateAPIView):
-    queryset = ParkingInvoice.objects.all()
+    queryset = Parking_Invoice.objects.all()
     serializer_class = ParkingInvoiceSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = CreateParkingInvoiceSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         invoice = serializer.save()
-        return Response(ParkingInvoiceSerializer(invoice).data)
+        return Response(
+            {
+                "invoice": ParkingInvoiceSerializer(invoice).data
+            },
+            status=status.HTTP_201_CREATED
+        )
 
 
 class ParkingInvoiceRetrieveUpdateView(generics.RetrieveUpdateAPIView):
-    queryset = ParkingInvoice.objects.all()
+    queryset = Parking_Invoice.objects.all()
     serializer_class = ParkingInvoiceSerializer
     lookup_field = 'invoice_id'
 
 
 class ParkingInvoiceDestroyView(generics.DestroyAPIView):
-    queryset = ParkingInvoice.objects.all()
+    queryset = Parking_Invoice.objects.all()
     serializer_class = ParkingInvoiceSerializer
     lookup_field = 'invoice_id'
 
 
 class SearchParkingInvoiceView(generics.ListAPIView):
-    queryset = ParkingInvoice.objects.all()
     serializer_class = ParkingInvoiceSerializer
 
     def get_queryset(self):
         search_term = self.request.GET.get('search', '')
-        return ParkingInvoice.objects.filter(record__record_id__icontains=search_term)
+        return Parking_Invoice.objects.filter(record__record_id__icontains=search_term)
