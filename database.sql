61d21251b13d584c86f356540b046c92bb1a55f7
QuangMinh2005 <minhdragonmcgamer@gmail.com>
2025-11-11 16:05:05 +0700

    Add initial data for Pricing and ParkingSlot tables
    Insert initial pricing data and parking slot configuration.

--- database.sql
+++ database.sql
@@ -124,3 +124,23 @@ CREATE TABLE parking_invoice (
     FOREIGN KEY (pricing_id) REFERENCES Pricing(pricing_id)
 );
 GO
+
+
+    
+Use ParkingManagement;
+GO
+
+INSERT INTO Pricing (pricing_id, vehicle_type, term, rate)
+VALUES
+('1', 'motorbike', 'hourly', 3000),   -- 3k/h
+('2', 'car', 'hourly', 10000),        -- 10k/h
+('3', 'motorbike', 'monthly', 300000),-- 300k/tháng
+('4', 'car', 'monthly', 1000000),     -- 1000k/tháng
+('5', 'motorbike', 'yearly', 800000), --800k/năm
+('6', 'car', 'yearly', 2000000); --2000k/năm
+GO
+
+-- Khởi tạo duy nhất 1 dòng cho bãi A, 100 chỗ
+INSERT INTO ParkingSlot (capacity, slots) VALUES (100, 100);
+GO
+
